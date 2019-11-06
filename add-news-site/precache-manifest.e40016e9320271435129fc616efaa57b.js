10cc4083de4cb348447c2e894e25281426f01162
MikhailK89 <55455174+MikhailK89@users.noreply.github.com>
2019-11-06 21:47:09 +0300

    done

---
+++ add-news-site/precache-manifest.e40016e9320271435129fc616efaa57b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5b87e251a77728a78e04d6f51aec30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea8aae1f14df354b1454",
+    "url": "./static/css/main.7e83fb6e.chunk.css"
+  },
+  {
+    "revision": "82ebd6ea06e90b4b74ce",
+    "url": "./static/js/2.1d105f2f.chunk.js"
+  },
+  {
+    "revision": "ea8aae1f14df354b1454",
+    "url": "./static/js/main.9590ef13.chunk.js"
+  },
+  {
+    "revision": "bc6d8f47f2157cbb55e7",
+    "url": "./static/js/runtime-main.724b164d.js"
+  }
+]);
