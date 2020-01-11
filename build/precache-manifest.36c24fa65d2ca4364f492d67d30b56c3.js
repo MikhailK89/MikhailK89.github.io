a4fcf31031d590b17c6b809af9f64a11c16249ab
MikhailK89 <55455174+MikhailK89@users.noreply.github.com>
2020-01-12 02:42:04 +0300

    test

---
+++ build/precache-manifest.36c24fa65d2ca4364f492d67d30b56c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3b3989558f0948d955116cac879852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be9428abbb1813596d9",
+    "url": "/static/css/main.0c288fef.chunk.css"
+  },
+  {
+    "revision": "b819d5251a6b1934112c",
+    "url": "/static/js/2.ef54b770.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ef54b770.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2be9428abbb1813596d9",
+    "url": "/static/js/main.b4555a96.chunk.js"
+  },
+  {
+    "revision": "2f3c2167d3920eeb25c9",
+    "url": "/static/js/runtime-main.9090ef1e.js"
+  },
+  {
+    "revision": "4efa0165426583a166d5262a41022ed2",
+    "url": "/static/media/documents.4efa0165.svg"
+  }
+]);
