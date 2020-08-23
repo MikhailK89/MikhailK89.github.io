85c5ccdec70f68179b0c996a296ddad4459e1b95
Mikhail <kravch_mikhail@mail.ru>
2020-08-23 22:53:42 +0300

    Add vue-crm site

---
+++ vue-crm-site/precache-manifest.c29356bcd1dd31a5994d97800804f8b6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3362b0c33d4f0247fb",
+    "url": "/css/app.e50ef0ad.css"
+  },
+  {
+    "revision": "bd589c025b9221fb9f2d",
+    "url": "/css/chunk-03ddbb00.a44684d1.css"
+  },
+  {
+    "revision": "becebc98b62078ad58e4",
+    "url": "/css/chunk-3adf0cb4.a2427d49.css"
+  },
+  {
+    "revision": "e5b0ad058e0bd2b2f6e8",
+    "url": "/css/chunk-8f8e2cc8.daf08dc4.css"
+  },
+  {
+    "revision": "6fda93581f4e993c3c64",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "65662e7778afaa0af9fd37904fb0ecc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3362b0c33d4f0247fb",
+    "url": "/js/app.d9d246cb.js"
+  },
+  {
+    "revision": "bd589c025b9221fb9f2d",
+    "url": "/js/chunk-03ddbb00.07449894.js"
+  },
+  {
+    "revision": "695b2287188f1f4be222",
+    "url": "/js/chunk-10b86dd9.0ac12b47.js"
+  },
+  {
+    "revision": "f27b30634cbbe93bb563",
+    "url": "/js/chunk-2d217a99.96030938.js"
+  },
+  {
+    "revision": "dae1ddfe2e373e8a6159",
+    "url": "/js/chunk-2d21a3d2.19e4d8b3.js"
+  },
+  {
+    "revision": "becebc98b62078ad58e4",
+    "url": "/js/chunk-3adf0cb4.b1c4721b.js"
+  },
+  {
+    "revision": "face128f39124a268f39",
+    "url": "/js/chunk-53c8ec65.63d7d97e.js"
+  },
+  {
+    "revision": "356b221ca8e656b94599",
+    "url": "/js/chunk-72449522.93991aa2.js"
+  },
+  {
+    "revision": "e5b0ad058e0bd2b2f6e8",
+    "url": "/js/chunk-8f8e2cc8.f65d54b8.js"
+  },
+  {
+    "revision": "d8e017f62264f3a32113",
+    "url": "/js/chunk-cfa57870.84ea2126.js"
+  },
+  {
+    "revision": "6fda93581f4e993c3c64",
+    "url": "/js/chunk-vendors.936e9bdf.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
